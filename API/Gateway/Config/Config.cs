e8e433c5e9453d273cedd708f32b0bef00b125fb
Jonathan Camilo Jiménez Ramos <jjimenez@aportesenlinea.com>
2021-04-20 19:22:58 -0500

    Redme

--- API/Gateway/Config/Config.cs
+++ API/Gateway/Config/Config.cs
@@ -11,20 +11,6 @@ namespace Properties.Gateway.Config
 
         public string PropertiesGrpc { get; set; }
 
-        public class LibrosOperations
-        {
-            public static string Get() => $"/api/v1/Libros/Get";
-
-            public static string GetById(int id) => $"/api/v1/Libros/Get/{id}";
-
-            public static string GetByEditorial(int id) => $"/api/v1/Libros/GetByEditorial/{id}";
-
-            public static string Create() => $"/api/v1/Libros/Create";
-
-            public static string Update() => $"/api/v1/Libros/Update";
-
-            public static string Delete(int id) => $"/api/v1/Libros/Delete/{id}";
-        }
 
         public class PropertyOperations
         {
@@ -37,19 +23,6 @@ namespace Properties.Gateway.Config
             public static string Update() => $"/api/v1/Properties/Update";
 
             public static string Delete(Guid id) => $"/api/v1/Properties/Delete/{id}";
-        }
-
-        public class EditorialesOperations
-        {
-            public static string Get() => $"/api/v1/Editoriales/Get";
-
-            public static string GetById(int id) => $"/api/v1/Editoriales/Get/{id}";
-
-            public static string Create() => $"/api/v1/Editoriales/Create";
-
-            public static string Update() => $"/api/v1/Editoriales/Update";
-
-            public static string Delete(int id) => $"/api/v1/Editoriales/Delete/{id}";
         }
     }
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Books
+# Properties
 
 Este es un proyecto de prueba para manejo de Propiedades, Dueños, etc. que implementa *.NET 5.0, SQL Server 2019 y Docker*.
 
@@ -10,12 +10,12 @@ Se debe clonar el repositorio y en la raíz de proyecto ejecutar:
 > docker-compose up -d
 
 O
-Luego se debe conectar a la base de datos y alguno de los archivos para restaura la DB *DB/Books/Restore*
+Luego se debe conectar a la base de datos y alguno de los archivos para restaura la DB *DB/PropertiesDB/Restore*
 
 Estos son los servicios que corren en el docker-compose:
 
-- Properties(Microservices): http://localhost:52964, https://localhost:44304
-- Gateway(API): http://localhost:50412, https://localhost:44333
+- Properties(Microservices): http://localhost:52964
+- Gateway(API): http://localhost:50412
 - SqlServer: "Server=localhost;User Id=sa;Password=12345678a"
 
 ## Repository
